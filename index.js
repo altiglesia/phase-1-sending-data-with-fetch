a132e28ac4bdc05f68acf8e5473a146898b27bdb
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-10-02 21:04:28 -0400

    commit tree 3dd5fe407e5277e55c6d07ada4296bc4f8a9fafc

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Add your code here
+// document.addEventListener("DOMContentLoaded", () => {
+//     const baseUrl = 'http://localhost:3000/users';
+    
+//         function submitData () {
+//             const formData = {
+//                 userName: `me123`,
+//                 userEmail: `me123@hotmail.com`,
+//             };
+        
+//             const configurationObject = {
+//                     method: "POST",
+//                     headers: {
+//                     "Content-Type": "application/json",
+//                     "Accept": "application/json",
+//                     },
+//                     body: JSON.stringify(formData),
+//                 };
+
+//             fetch(baseUrl, configurationObject)
+//             .then(function (response) {
+//             return response.json();
+//             })
+//             .then(function (object) {
+//                 console.log(object);
+//             })
+//             .catch(function (error) {
+//                 alert("Bad things!");
+//             return fetch
+//         });
+//     }
+// })
+
+function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+      .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }
